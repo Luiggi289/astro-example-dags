7a7ae75fd64a76feecb8cb7825eacb451d76578a
Luiggi289 <moralesmeza28@gmail.com>
2024-01-22 00:59:08 -0500

    Add files via upload

--- dags/s3_transform_2.py
+++ dags/s3_transform_2.py
@@ -4,9 +4,32 @@ from airflow.models.connection import Connection
 from time import time_ns
 from datetime import datetime
 import os
+from pymongo import MongoClient
+from pandas import DataFrame
+import pandas as pd
 
+def get_connect_mongo():
+
+    CONNECTION_STRING ="mongodb+srv://atlas:T6.HYX68T8Wr6nT@cluster0.enioytp.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING)
+
+    return client
+    
 def flujo_envio():
     print(" exitosamente!")
+    
+
+def load_products():
+    dbconnect = get_connect_mongo()
+    dbname=dbconnect["retail_db"]
+    collection_name = dbname["products"] 
+    products = collection_name.find({})
+    products_df = DataFrame(products)
+    dbconnect.close()
+    products_df['_id'] = products_df['_id'].astype(str)
+    products_df['product_description'] = products_df['product_description'].astype(str)
+    products_rows=len(products_df)
+    print(f" Se obtuvo  {products_rows}  Filas")
 
 with DAG(
     dag_id="test_print", schedule="@once", start_date=datetime(2023, 2, 1), is_paused_upon_creation=False, catchup=False
@@ -16,9 +39,9 @@ with DAG(
         python_callable=flujo_envio,
         dag=dag
     )
-    envio_correo_2 = PythonOperator(
-        task_id='envio_correo_2',
+    step_load = PythonOperator(
+        task_id='load_products',
         python_callable=flujo_envio,
         dag=dag
     )
-    envio_correo>>envio_correo_2
+    envio_correo>>step_load
