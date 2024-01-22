1b966a2b217c1e0c0fe41ad0dc10146e33ca6b90
Luiggi289 <moralesmeza28@gmail.com>
2024-01-22 00:04:54 -0500

    Add files via upload

--- dags/s3_transform_2.py
+++ dags/s3_transform_2.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.models.connection import Connection
 from time import time_ns
 from datetime import datetime
