cab759bccb1dd7cd6cd69e1600c4790b717a41e0
Luiggi289 <moralesmeza28@gmail.com>
2024-01-22 10:58:24 -0500

    Add files via upload

---
+++ dags/01_mi_primer_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.connection import Connection
+
+
+    
+def start_process():
+    print(" INICIO EL PROCESO!")
+
+def end_process():
+    print(" FIN DEL PROCESO!")
+
+def load_products():
+    print(" Hola Airflow!")
+
+with DAG(
+    dag_id="mi_primer_dag", schedule="@once", start_date=datetime(2024, 1, 22), is_paused_upon_creation=False, catchup=False
+) as dag:
+    step_start = PythonOperator(
+        task_id='step_start_id',
+        python_callable=start_process,
+        dag=dag
+    )
+    step_load = PythonOperator(
+        task_id='load_products_id',
+        python_callable=load_products,
+        dag=dag
+    )
+    step_end = PythonOperator(
+        task_id='step_end_id',
+        python_callable=end_process,
+        dag=dag
+    )
+    step_start>>step_load>>step_end
